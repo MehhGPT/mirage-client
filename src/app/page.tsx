b4abfde1bc6652b639dcab482c3777628837883b
PATEL96 <patel.raj.official.96@gmail.com>
2024-07-27 16:55:38 +0530

    feat: minor changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Navbar } from "./HomePage/navbar";
+import MangaCard from "@/components/mangacard";
+import { Slider } from "@/components/slider";
+import Cover from "@/images/COVER1.jpg";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
+    <div className="flex items-center justify-center flex-col">
+      <Slider />
+      <div className=" flex flex-wrap flex-row w-[90%] items-center justify-center">
+        <MangaCard Desc="Berserk is a Japanese manga series written and illustrated by Kentaro Miura" image={Cover} Title="Beserk Vol 1" />
+        <MangaCard Desc="Berserk is a Japanese manga series written and illustrated by Kentaro Miura" image={Cover} Title="Beserk Vol 2" />
+        <MangaCard Desc="Berserk is a Japanese manga series written and illustrated by Kentaro Miura" image={Cover} Title="Beserk Vol 3" />
+        <MangaCard Desc="Berserk is a Japanese manga series written and illustrated by Kentaro Miura" image={Cover} Title="Beserk Vol 4" />
+        <MangaCard Desc="Berserk is a Japanese manga series written and illustrated by Kentaro Miura" image={Cover} Title="Beserk Vol 5" />
+        <MangaCard Desc="Berserk is a Japanese manga series written and illustrated by Kentaro Miura" image={Cover} Title="Beserk Vol 6" />
+      </div>
     </div>
   );
 }
