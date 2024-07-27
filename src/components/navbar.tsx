4754cf18bb9b77836431295265317c34877dcbf4
PATEL96 <patel.raj.official.96@gmail.com>
2024-07-27 17:13:39 +0530

    feat: minor changes.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,7 +20,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 export function Navbar() {
     return (
         <div className="flex h-[10svh] w-full z-0 flex-col">
-            <div className="fixed top-0 w-full flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <div className="fixed top-0 w-full flex h-16 shadow-md items-center gap-4 border-b bg-background px-4 md:px-6">
                 <nav className="hidden flex-col gap-6 text-lg font-normal md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                     <Link
                         href="#"
