651336f21d83f1c69bf328e0655d0edf7d827453
PATEL96 <patel.raj.official.96@gmail.com>
2024-07-27 17:27:43 +0530

    feat: Added new Hover effects on the card.

--- src/components/mangacard.tsx
+++ src/components/mangacard.tsx
@@ -11,7 +11,7 @@ interface CusProps {
 export default function MangaCard({ Title, Desc, image, link }: CusProps) {
     return (
         <Link href={link}>
-            <div className="relative group cursor-pointer overflow-hidden text-gray-50 h-72 w-max rounded hover:duration-500 duration-500 m-[1rem]">
+            <div className="relative group hover:shadow-xl cursor-pointer overflow-hidden text-gray-50 h-72 w-max rounded hover:duration-500 duration-500 m-[1rem]">
                 <div className="w-56 h-72 text-gray-800">
                     <div className="flex flex-row justify-between z-[5]">
                         <svg
@@ -49,7 +49,7 @@ export default function MangaCard({ Title, Desc, image, link }: CusProps) {
                     </div>
                     <Image src={image} alt="Beserk" className=" absolute top-0 -z-[1] group-hover:grayscale group-hover:duration-500 duration-500" />
                 </div>
-                <div className="absolute bg-gray-50 group-hover:bg-[#cccccca2] group-hover:backdrop-blur-md -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-500 duration-500">
+                <div className="absolute bg-gray-50 shadow-md shadow-black group-hover:bg-[#cccccca2] group-hover:backdrop-blur-md -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-500 duration-500">
                     <span className="text-lime-400 font-bold text-xs"></span>
                     <span className="text-gray-800 font-bold text-3xl">{Title}</span>
                     <p className="text-neutral-800">
